7d5321bdea8ebd9b7aa74bbafa484fb8b251ab36
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-18 14:03:31 +0100

    try more on ReactState

--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -4,17 +4,26 @@ let [important,setIsImportant] = React.useState(0)
 
     
 function increment(){
-    setIsImportant(important + 1)
+    setIsImportant(prevCount => prevCount + 1)
     console.log("increment button was clicked")
 }
 function decrement(){
-    setIsImportant(important - 1)
-    console.log("decrement button was clicked")
+    setIsImportant(function (prevCount){
+        return prevCount - 1
+    })
+    
 }
+// let answer = isGoingOut != "i want to go out" ? "This true" : "This is false"
+    const [isGoingOut, setGoingOut] = React.useState(true)
+    function answer(){
+        setGoingOut((prevState => !prevState))
+    }
 
     return(
         <div>
            <h1> {important} </h1>
+           <button onClick={answer}> Going Out</button>
+           <h1>{isGoingOut ? "Yes" : "No"}</h1>
           <button className="addition" onClick={increment}> + </button>
           <button className="substraction" onClick={decrement}>-</button>
 
